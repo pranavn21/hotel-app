b9d79aa6c4db2848b286fa65fc163707d5f689c6
pranavn21 <pxn190027@utdallas.edu>
2025-02-23 21:57:24 -0600

    introducing CRUD functions

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
+  private reservations: Reservation[] = [];
+  // CRUD
 
-  constructor() { }
+  getReservations(): Reservation[]{
+    return this.reservations;
+  }
+
+  getReservation(id: string): Reservation | undefined{
+    return this.reservations.find(res => res.id === id);
+  }
 }
