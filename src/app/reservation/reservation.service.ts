e1cfb368d4ed2b5203abcf19c713f8a851db6ba4
pranavn21 <pxn190027@utdallas.edu>
2025-02-23 22:04:21 -0600

    Completed basic CRUD operations implementation for ReservationService

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -15,4 +15,18 @@ export class ReservationService {
   getReservation(id: string): Reservation | undefined{
     return this.reservations.find(res => res.id === id);
   }
+
+  addReservation(reservation: Reservation): void {
+    this.reservations.push(reservation);
+  }
+
+  deleteReservation(id: string): void{
+    let index = this.reservations.findIndex(res => res.id === id);
+    this.reservations.splice(index,1) // splice at index and remove one element, remove that element at index
+  }
+
+  updateReservation(updatedReservation: Reservation): void{
+    let index = this.reservations.findIndex(res => res.id === updatedReservation.id);
+    this.reservations[index] = updatedReservation;
+  }
 }
